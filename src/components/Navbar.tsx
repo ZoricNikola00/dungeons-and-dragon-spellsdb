d743214372a48b1a57db35948527ecb7859fe2b5
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-04 15:06:30 +0100

    Create and edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {GiScrollQuill} from 'react-icons/gi'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='fixed top-0 left-0 w-full text-white z-10 bg-[#666633] shadow shadow-[#666633] py-4 '>
+        <div className='flex justify-between items-center w-[90%] md:w-[80%] mx-auto'>
+            <h1 className='text-xl md:text-3xl font-bold cursor-pointer'>Dungeons & Dragons SpellsDB</h1>
+            <GiScrollQuill className='text-4xl'/>
+        </div>
+    </nav>
   )
 }
