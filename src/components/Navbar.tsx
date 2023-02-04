0725fc5b791bbc5d49af36d175c2a20a8d58741c
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-04 15:25:20 +0100

    Add nav for home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import {GiScrollQuill} from 'react-icons/gi'
 
 export const Navbar = () => {
+    const nav=useNavigate()
+
   return (
     <nav className='fixed top-0 left-0 w-full text-white z-10 bg-[#666633] shadow shadow-[#666633] py-4 '>
         <div className='flex justify-between items-center w-[90%] md:w-[80%] mx-auto'>
-            <h1 className='text-xl md:text-3xl font-bold cursor-pointer'>Dungeons & Dragons SpellsDB</h1>
+            <h1 className='text-xl md:text-3xl font-bold cursor-pointer' onClick={()=>nav('/')}>Dungeons & Dragons SpellsDB</h1>
             <GiScrollQuill className='text-4xl'/>
         </div>
     </nav>
