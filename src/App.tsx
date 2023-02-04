ab86fde433b0e7bb71757f6531583c9014d98ba5
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-04 15:10:27 +0100

    Add background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Background from './components/Background'
 import { Navbar } from './components/Navbar'
 
 const App = () => {
   return (
-    <main className='w-full'>
+    <main className='w-full mt-[100px]'>
       <Navbar/>
+      <Background/>
     </main>
   )
 }
