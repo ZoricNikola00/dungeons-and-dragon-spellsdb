d816e78ad3518ce9cdd0f2b532d079b2ac599ef8
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-05 22:32:44 +0100

    Minor edit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -14,7 +14,7 @@ const Pagination = ({spellsPerPage, totalSpells,currentPage,setCurrentPage}:pagi
   return (
         <ul className='text-white flex items-center justify-center text-lg md:text-3xl mb-4 md:mb-0'>
             {pageNumbers.map(number=>(
-                <li key={number} onClick={()=>setCurrentPage(number)} className={`${currentPage===number?'bg-[#666633]':'bg-transparent'} rounded p-3 md:p-4 hover:bg-[#666633] cursor-pointer m-1 md:m-2`}>
+                <li key={number} onClick={()=>setCurrentPage(number)} className={`${currentPage===number?'bg-[#666633]':'bg-transparent'} rounded p-3 md:p-4 transition duration-300 hover:bg-[#666633] cursor-pointer m-1 md:m-2`}>
                     {number}
                 </li>
             ))}
