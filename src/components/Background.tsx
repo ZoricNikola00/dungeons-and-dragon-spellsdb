ff0aabaf7eaf8afc994943956000098f0447165e
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-05 20:59:08 +0100

    Edit for test

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import bg from '../images/bg.avif'
 const Background = () => {
   return (
     <>
         <div className='fixed top-0 left-0 w-full h-full bg-black/70 -z-10'/>
-        <img src={bg} alt='background' className='fixed w-full top-0 left-0 h-full object-cover -z-20'/>
+        <img src={bg} data-testid='background' alt='background' className='fixed w-full top-0 left-0 h-full object-cover -z-20'/>
     </>
   )
 }
