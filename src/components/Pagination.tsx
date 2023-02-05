b5bab3bf2ba4aa795b2514b984f26f24e0faa23c
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-05 22:15:26 +0100

    Creating and editing pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+type pagination={
+    spellsPerPage:number;
+    totalSpells:number;
+    currentPage:number;
+    setCurrentPage:(n:number)=>void
+}
+const Pagination = ({spellsPerPage, totalSpells,currentPage,setCurrentPage}:pagination) => {
+    const pageNumbers = [];
+
+    for (let i=1;i<=Math.ceil(totalSpells/spellsPerPage);i++){
+        pageNumbers.push(i)
+    }
+  return (
+        <ul className='text-white flex items-center justify-center text-lg md:text-3xl mb-4 md:mb-0'>
+            {pageNumbers.map(number=>(
+                <li key={number} onClick={()=>setCurrentPage(number)} className={`${currentPage===number?'bg-[#666633]':'bg-transparent'} rounded p-3 md:p-4 hover:bg-[#666633] cursor-pointer m-1 md:m-2`}>
+                    {number}
+                </li>
+            ))}
+        </ul>
+  )
+}
+
+export default Pagination
