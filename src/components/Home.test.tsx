1ad7215e2718fe899cffd09a0c06af3ad729a902
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-05 20:58:47 +0100

    Write test for home component

---
+++ src/components/Home.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import Home from './Home'
+import {useNavigate} from 'react-router-dom'
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: jest.fn(),
+  }
+})
+
+describe('Home component', () => {
+  it('should render title and button', () => {
+    const {getByText, getByTestId} = render(<Home />)
+
+    const title = getByText('Welcome to the Dungeons & Dragons SpellsDB')
+    const button = getByTestId('check-spells')
+
+    expect(title).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+  })
+
+  it('should navigate to spells page when button is clicked', () => {
+    const navigate = jest.fn()
+    ;(useNavigate as jest.Mock).mockReturnValue(navigate)
+
+    const {getByTestId} = render(<Home />)
+
+    const button = getByTestId('check-spells')
+
+    fireEvent.click(button)
+
+    expect(navigate).toHaveBeenCalledWith('/spells')
+  })
+})
