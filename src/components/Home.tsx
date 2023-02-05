3ea0102f87768b09a302a894d0d240075c93c107
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-05 20:58:12 +0100

    Edit component for test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
 const Home = () => {
-    const nav=useNavigate()
+  const nav=useNavigate()
   return (
     <div className='flex justify-center items-center gap-4 text-white flex-col mt-[200px]'>
         <h1 className='text-5xl text-center'>Welcome to the Dungeons & Dragons SpellsDB</h1>
-        <button className='text-2xl border border-[#666633] transition duration-500 hover:bg-white hover:text-[#666633] bg-[#666633] p-4 tracking-wider rounded-lg' onClick={()=>nav('/spells')}>Check the spells</button>
+        <button data-testid='check-spells' className='text-2xl border border-[#666633] transition duration-500 hover:bg-white hover:text-[#666633] bg-[#666633] p-4 tracking-wider rounded-lg' onClick={()=>nav('/spells')}>Check the spells</button>
     </div>
   )
 }
