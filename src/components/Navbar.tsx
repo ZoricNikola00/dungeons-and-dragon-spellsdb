4073f67c9366bc35932d6bf2db0deca944aec548
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-17 00:52:56 +0100

    Minor edit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <nav className='fixed top-0 left-0 w-full text-white z-10 bg-[#666633] shadow shadow-[#666633] py-3'>
         <div className='flex justify-between items-center w-[90%] md:w-[80%] mx-auto'>
             <h1 className='text-xl md:text-3xl font-bold cursor-pointer' onClick={()=>nav('/')}>Dungeons & Dragons SpellsDB</h1>
-            <GiScrollQuill data-testid='icon' onClick={()=>nav('/favorite')} className='text-5xl cursor-pointer'/>
+            <GiScrollQuill data-testid='icon' onClick={()=>nav('/favorite')} className='text-5xl cursor-pointer transition duration-300 hover:text-green-300/30'/>
         </div>
     </nav>
   )
