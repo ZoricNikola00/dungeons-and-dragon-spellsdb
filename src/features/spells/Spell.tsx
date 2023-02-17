fe4923f2f9b9ba2a0686fe8f4c9e5fceaae2ad18
ZoricNikola00 <zoriclfc@gmail.com>
2023-02-17 13:38:15 +0100

    Edit component for testing

--- src/features/spells/Spell.tsx
+++ src/features/spells/Spell.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect } from 'react'
 import {useParams} from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../hooks/spellHooks'
@@ -19,35 +20,35 @@ const Spell = () => {
     },[])
 
     if(isLoading){
-        return <RingLoader color='#666633' size={150} className='my-[200px] w-[full] mx-auto'/>
+        return <RingLoader data-testid='loader' color='#666633' size={150} className='my-[200px] w-[full] mx-auto'/>
     }
   return (
     <div className='relative w-[90%] md:w-[80%] bg-green-300/30 rounded p-4 text-white mx-auto my-[100px]'>
         <button onClick={()=>nav('/spells')} className='flex items-center gap-2 text-lg text-gray-300 italic p-2 border border-transparent rounded transition duration-300 hover:border-gray-300'><ImArrowLeft/> Back</button>
         <div className='flex items-center justify-between text-4xl py-2'>
-            <h1 className='font-bold'>{spell?.name}</h1>
-            <button onClick={_=>dispatch(addFavorite(spell))}><FaHeart className={`${checkFavorite?'text-[#666633]':'text-white/30'} transition duration-500`}/></button>
+            <h1 data-testid='name' className='font-bold'>{spell?.name}</h1>
+            <button data-testid='favorite' onClick={_=>dispatch(addFavorite(spell))}><FaHeart className={`${checkFavorite?'text-[#666633]':'text-white/30'} transition duration-500`}/></button>
         </div>
         <hr/>
         <div className='text-sm md:text-lg grid grid-cols-2 md:grid-cols-4 p-4 gap-2'>
             <div className={styledItem.div}><span className={styledItem.span}>Level: </span><br className={styledItem.br}/>{spell?.level}</div>
-            <div className={styledItem.div}><span className={styledItem.span}>Components: </span><br className={styledItem.br}/>{spell?.components.map((comp,i)=><span key={i} className='mr-1'>{comp}</span>)}</div>
+            <div className={styledItem.div}><span className={styledItem.span}>Components: </span><br className={styledItem.br}/>{spell?.components?.map((comp,i)=><span key={i} className='mr-1'>{comp}</span>)}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Range: </span><br className={styledItem.br}/>{spell?.range||'/'}</div>
-            <div className={styledItem.div}><span className={styledItem.span}>Classes: </span><br className={styledItem.br}/>{spell?.classes.map(clas=><span key={clas.index} className='mr-1'>{clas.name}</span>)}</div>
+            <div className={styledItem.div}><span className={styledItem.span}>Classes: </span><br className={styledItem.br}/>{spell?.classes?.map(clas=><span key={clas.index} className='mr-1'>{clas.name}</span>)}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Casting Time: </span><br className={styledItem.br}/>{spell?.casting_time}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Concentration: </span><br className={styledItem.br}/>{spell?.concentration?"NO":"YES"}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Duration: </span><br className={styledItem.br}/>{spell?.duration}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Type of attack: </span><br className={styledItem.br}/>{spell?.attack_type|| '/'}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Material: </span><br className={styledItem.br}/>{spell?.material || '/'}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Damage: </span><br className={styledItem.br}/>{spell?.damage?.damage_type_name || '/'}</div>
-            <div className={styledItem.div}><span className={styledItem.span}>School: </span><br className={styledItem.br}/>{spell?.school.name}</div>
+            <div className={styledItem.div}><span className={styledItem.span}>School: </span><br className={styledItem.br}/>{spell?.school?.name}</div>
             <div className={styledItem.div}><span className={styledItem.span}>Ritual: </span><br className={styledItem.br}/>{spell?.ritual?"YES":"NO"}</div>
         </div>
         <hr/>
         <div className='p-1'>
             <h1 className='font-semibold'>Description: </h1>
             <div className='p-2'>
-                {spell?.desc.map((des,i)=>(
+                {spell?.desc?.map((des,i)=>(
                     <p key={i} className='mb-3'>
                         {des}
                     </p>
